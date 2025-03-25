51091ef225b635a513039a2ec998a4bcd6447c1f
Roshan naik <naikr79972@gmail.com>
2025-03-25 17:14:51 +0530

    added error if feth fails

--- ecom/src/App.jsx
+++ ecom/src/App.jsx
@@ -1,39 +1,60 @@
-import React ,{useState}from 'react'
-import Header from './Components/Header'
-import MovieList from './Components/MovieList'
+import React, { useState } from 'react';
+import Header from './Components/Header';
+import MovieList from './Components/MovieList';
 
 const App = () => {
-  const[movies,setMovies]=useState([]);
-  const[isLoading,setIsLoading]=useState(false);
-  async function fetchMoviesHandler(){
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchMoviesHandler() {
     setIsLoading(true);
-  const response=await fetch('https://swapi.dev/api/films/');
-  const data=await response.json();
-    const transformedMovies=data.results.map(movieData=>{
-      return {
-        id:movieData.episode_id,
-        title:movieData.title,
-        openingText:movieData.opening_crawl,
-     releaseData: movieData.release_date
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Something went wrong. Status code: ' + response.status);
       }
-    });
-   setMovies(transformedMovies);
-   setIsLoading(false);
-  
+
+      const data = await response.json(); // This is where the error occurs
+      setMovies(data.results.map(movieData => ({
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseData: movieData.release_date
+      })));
+    } catch (error) {
+      setError(`Failed to fetch movies:`);
+    } finally {
+      setIsLoading(false);
+    }
   }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MovieList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <div>
-    <Header/>
-    <section>
-      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section> 
+      <Header />
       <section>
-      {!isLoading && movies.length > 0 && <MovieList movies={movies}/>}
-      {!isLoading && movies.length===0 && <p>Found no movies</p>}
-      {isLoading && <p>Loading...</p>}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {content}
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
