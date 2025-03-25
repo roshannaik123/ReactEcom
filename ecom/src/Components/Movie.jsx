70af22bd8d0512c4b0175657fa2f76c385bc0969
Roshan naik <naikr79972@gmail.com>
2025-03-25 11:50:31 +0530

    fetch movieList using fetch api

--- ecom/src/Components/Movie.jsx
+++ ecom/src/Components/Movie.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const Movie = (props) => {
   return (
     <div>
-    <li>
-        {props.title}
-    </li>
+    <li>{props.title}</li>
     <h2>{props.title}</h2>
     <h3>{props.releaseDate}</h3>
     <p>{props.openingText}</p>
